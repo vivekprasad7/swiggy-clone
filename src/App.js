55f70d08b09186f2c57b9baa9e51cb3812b50024
Vivek Prasad <flashtech007@gmail.com>
2024-01-27 08:31:17 +0530

    updated App.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+
+const App = () => {
+    return(
+        <div>
+            <h1>Hello React</h1>
+        </div>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(<App/>)
