e628e80b388411463d33680ebd4f1d78601feefe
Vivek Prasad <flashtech007@gmail.com>
2024-01-27 13:23:26 +0530

    feat - fetched images from api

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import "./Card.css"
+import { imageURL } from '../../utils/constants'
 
 
 const Card = ({item}) => {
+    console.log(item)
+
   return (
     <div className='card'>
+        <img src={imageURL+item?.info?.cloudinaryImageId} alt="" />
         <p>{item?.info?.name}</p>
         <p>Locality: {item?.info?.locality}</p>
     </div>
