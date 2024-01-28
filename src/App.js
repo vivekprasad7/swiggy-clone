40f8954afd7fa16931a75cc7e03c48ed12d27562
Vivek Prasad <flashtech007@gmail.com>
2024-01-28 06:15:59 +0530

    added outlet for children

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client"
 import Header from "./components/header/Header";
 import Body from "./components/body/Body";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";  
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";  
 import About from "./pages/about/About";
 import Contact from "./pages/contact/Contact";
 import Cart from "./pages/cart/Cart";
@@ -13,7 +13,7 @@ const App = () => {
     return(
         <div>
             <Header/>
-            <Body/>
+            <Outlet/>
         </div>
     )
 }
@@ -23,6 +23,10 @@ const appRouter = createBrowserRouter([
         path:"/",
         element:<App/>,
         children: [
+            {
+                path:"/",
+                element: <Body/>,
+            },
             {
                 path:"/about",
                 element: <About/>,
