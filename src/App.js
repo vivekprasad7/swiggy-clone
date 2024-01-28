2c311ef915b63c61c5904ae105136f0135f36904
Vivek Prasad <flashtech007@gmail.com>
2024-01-28 05:40:12 +0530

    created pages and setup react-router-dom 1. Create Pages 2. import createBrowserRouter and RouterProvider 3. Configure appRouter 4. Provide (appRouter) using RouterProvider

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client"
 import Header from "./components/header/Header";
 import Body from "./components/body/Body";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";  // 1
+import About from "./pages/about/About";
 
 
 const App = () => {
@@ -13,6 +15,31 @@ const App = () => {
     )
 }
 
+//  2
+const appRouter = createBrowserRouter([
+    {
+        path:"/",
+        element:<App/>,
+    },
+    {
+        path:"/about",
+        element: <About/>,
+    },
+    {
+        path:"/contact",
+        element: <About/>,
+    },
+    {
+        path:"/cart",
+        element: <About/>,
+    },
+
+])
+
+
+
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-root.render(<App/>)
+root.render(<RouterProvider router={appRouter}/>) // 3
