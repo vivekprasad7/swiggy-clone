37837cb6ce0798215d92c4bcf0fcdcc5a785615e
Vivek Prasad <flashtech007@gmail.com>
2024-02-13 18:20:22 +0530

    adding state to class based component

--- src/classComponents/UserClass.jsx
+++ src/classComponents/UserClass.jsx
@@ -1,16 +1,22 @@
 import React from "react";
+import "./UserClass.css"
 
 class UserClass extends React.Component {
 
     constructor(props){
         super(props)
         console.log("prop", props)
+
+        this.state = {
+            count:0,
+        }
     }
 
     render() {
         const { name} = this.props;
         return (
             <div className="user-card">
+                <h1>Count: {this.state.count}</h1>
                 <h2>Name: {name}</h2>
                 <h3>Location: Himachal</h3>
             </div>
--- src/components/user/User.jsx
+++ src/components/user/User.jsx
@@ -1,6 +1,11 @@
+import { useState } from "react"
+
 const User = (props) => {
+
+    const [ count] = useState(0)
     return(
         <div className="user-card">
+            <h1>Count: {count}</h1>
             <h1>Name: {props.name}</h1>
             <h2>Car: Ferrari</h2>
             <h2>Dog: Baadal</h2>
